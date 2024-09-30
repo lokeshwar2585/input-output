5503770b8716dfa462f614d634ceeab12ac29ecc
lokeshwar2585 <114808602+lokeshwar2585@users.noreply.github.com>
2024-09-30 11:10:04 +0530

    Add files via upload

---
+++ deviage.py
@@ -0,0 +1,3 @@
+cAge=int(input())
+deviAge =((cAge+3)*4)-3
+print(deviAge)
---
+++ percentageOfIncome.py
@@ -0,0 +1,20 @@
+#Tamilnadu was battling one if it’s worst floods in a century last December, 
+#as several part of the state have been submerged and cut off from essential supplies. 
+#It was heartening that the Cricketers came forward to contribute for the cause of floods 
+#and it was decided amongst the team that the senior players donate 50% of their salary 
+#and junior players to donate 40% against the flood relief measures
+#Assume there are 6 senior players and 5 junior players. 
+#The salary of senior players Rs.X and that of junior players 
+#is Rs.Y. Find the total contribution from the cricket team towards the floods.
+#Ans:
+X=int(input())
+Y=int(input())
+t1=X*.50*6
+t2=Y*.40*5
+t=(t1+t2)
+print("%.2f"%t)
+
+
+
+
+
